a3850bf87fd0b7e8e99bf5723205036ae4daa1b5
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-12-12 20:02:29 -0500

    Se organiza el caso de score diferente

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -3,7 +3,7 @@ public class TennisGame2 implements TennisGame
 {
     public int p1Point = 0;
     public int p2Point = 0;
-    
+
     public String p1Res = "";
     public String p2Res = "";
     private String player1Name;
@@ -22,58 +22,9 @@ public class TennisGame2 implements TennisGame
         else if (p1Point >= 4 || p2Point >=4)
             score = scoreGreaterThan4();
 
-
+        else
+            score = differentScore(p1Point).concat("-").concat(differentScore(p2Point));
         
-        if (p1Point > 0 && p2Point ==0)
-        {
-            if (p1Point ==1)
-                p1Res = "Fifteen";
-            if (p1Point ==2)
-                p1Res = "Thirty";
-            if (p1Point ==3)
-                p1Res = "Forty";
-            
-            p2Res = "Love";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point > 0 && p1Point ==0)
-        {
-            if (p2Point ==1)
-                p2Res = "Fifteen";
-            if (p2Point ==2)
-                p2Res = "Thirty";
-            if (p2Point ==3)
-                p2Res = "Forty";
-            
-            p1Res = "Love";
-            score = p1Res + "-" + p2Res; //Caso cero
-        }
-        
-        if (p1Point > p2Point && p1Point < 4)
-        {
-            if (p1Point ==2)
-                p1Res ="Thirty";
-            if (p1Point ==3)
-                p1Res ="Forty";
-            if (p2Point ==1)
-                p2Res ="Fifteen";
-            if (p2Point ==2)
-                p2Res ="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point > p1Point && p2Point < 4)
-        {
-            if (p2Point ==2)
-                p2Res ="Thirty";
-            if (p2Point ==3)
-                p2Res ="Forty";
-            if (p1Point ==1)
-                p1Res ="Fifteen";
-            if (p1Point ==2)
-                p1Res ="Thirty";
-            score = p1Res + "-" + p2Res; //Caso diferente menor a 4
-        }
-
 
         return score;
     }
@@ -103,6 +54,15 @@ public class TennisGame2 implements TennisGame
         return minusResult >= 2 ? "Win for player1": "Win for player2";
     }
 
+    private String differentScore(int scorePlayer) {
+        switch(scorePlayer)
+        {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            default: return "Forty";
+        }
+    }
 
     public void P1Score(){
         p1Point++;
